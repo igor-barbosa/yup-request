7b1b89a61595cf81275b8760ad3519f96186cfd7
Igor Barbosa <igor.barbosa.dev@gmail.com>
2020-05-10 13:50:51 -0300

    Criando função createMiddlware

--- src/http/requests/createUserRequest.js
+++ src/http/requests/createUserRequest.js
@@ -1,54 +1,7 @@
 import * as yup from 'yup';
 import YupRequest from "../../libs/YupRequest/YupRequest";
-import nested from 'nested-property';
 
-function makeValidationRequest(schema, customValidation = null) {
-    return async function (req, res, next) {
-
-        const util = {
-            label: (path) => {
-                if(!!req.body.__labels__ && !!req.body.__labels__[path]){
-                    return req.body.__labels__[path];
-                } else {
-                    return path;
-                }
-            },
-            errors: (data) => ({
-                error: true,
-                type: 'fields',
-                data: data.map(fieldError => ({
-                    path: fieldError.path,
-                    value: nested.get(req.body,fieldError.path),
-                    message: fieldError.message,
-                    type: 'custom'
-                })) 
-            }),
-            errorText: (message) => {
-                return {
-                    error: true,
-                    type: 'text',
-                    data: message
-                }
-            }
-        }
-
-        const validate = new YupRequest(req.body, schema);    
-        const {error, data} = await validate.run();    
-        if(error){
-            res.send({ error, data });
-        } else {
-            const resp = (!!customValidation) ? customValidation(data, util) : { error : false };
-            if(resp.error){
-                res.send(resp);
-            } else {
-                req.body = data;
-                next();
-            }
-        }
-    }
-}
-
-const createUserRequest = makeValidationRequest({
+const createUserRequest = YupRequest.createMiddleware({
     name: yup.string().cpf().required()
 });
 
--- src/libs/YupRequest/YupRequest.js
+++ src/libs/YupRequest/YupRequest.js
@@ -1,11 +1,58 @@
 import * as yup from 'yup';
 import './locale';
+import nested from 'nested-property';
 
 export default class YupRequest {
     
     constructor(body, schema) {
         this.body = body;
         this.schema = yup.object().shape(schema);
+    }
+
+    static createMiddleware(schema, customValidation = null) {
+        return async function (req, res, next) {
+    
+            const util = {
+                label: (path) => {
+                    if(!!req.body.__labels__ && !!req.body.__labels__[path]){
+                        return req.body.__labels__[path];
+                    } else {
+                        return path;
+                    }
+                },
+                errors: (data) => ({
+                    error: true,
+                    type: 'fields',
+                    data: data.map(fieldError => ({
+                        path: fieldError.path,
+                        value: nested.get(req.body,fieldError.path),
+                        message: fieldError.message,
+                        type: 'custom'
+                    })) 
+                }),
+                errorText: (message) => {
+                    return {
+                        error: true,
+                        type: 'text',
+                        data: message
+                    }
+                }
+            }
+    
+            const validate = new YupRequest(req.body, schema);    
+            const {error, data} = await validate.run();    
+            if(error){
+                res.send({ error, data });
+            } else {
+                const resp = (!!customValidation) ? customValidation(data, util) : { error : false };
+                if(resp.error){
+                    res.send(resp);
+                } else {
+                    req.body = data;
+                    next();
+                }
+            }
+        }
     }
 
     async run() {
